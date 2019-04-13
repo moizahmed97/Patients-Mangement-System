7f81ef9afc922979d8198abddd2d20da34e71ac5
moiz <moizahmed97@gmail.com>
2019-04-13 19:39:49 +0300

    Create pages for our 3 clinics

---
+++ html/appointmentBooking.php
@@ -0,0 +1,7 @@
+// Form that has name (autofilled using $session[name] )
+// Date Given by user
+// Name of the dentist autofilled from $session[dentistSelected]
+
+
+
+// This page is opened when the user clicks a dentist Card
