afb4b3069e0a6905b5039eb931bb06b01cfafd9c
junaidmy <48646123+junaidmy@users.noreply.github.com>
2019-04-15 19:27:16 +0300

    Add files via upload

---
+++ php files/confirm.php
@@ -0,0 +1,19 @@
+<?php
+require_once("dbconfig.php");
+if(isset($_POST['Confirm']))
+	{
+		$Apm_ID=$_POST["Apm_ID"];
+		$time = $_POST["time"];	
+		$updatequery="update Appointment set Status='Confirmed' AND Time = '$time' where Apm_ID = '$Apm_ID';
+		if (mysqli_query($conn, $updatequery)) 
+			{
+				echo "$fnm[$j] :Status updated successfully<br>";
+			} 
+		else
+			{
+				echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+			}
+		echo "Redirecting.....";
+		header( "Refresh:3; url=receptionist-dashboard.html");		
+	}
+?>
